90f132ff5f198d77a82d9566d5383fc3dd835aaa
DevTadeu <99771087+DevTadeu@users.noreply.github.com>
2022-03-03 13:51:07 -0300

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,16 @@
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# E se eu quisesse iniciar os meses em uma nova linha?
+# Simplesmente comece a string com \n
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+print("I am 6'2\" tall.")  # escape de aspas duplas dentro da string
+print('I am 6\'2" tall.')  # escape de aspas simples dentro da string
