cafada7dcb0cbfc548e97edcecc0bf95bcd01faa
DevTadeu <99771087+DevTadeu@users.noreply.github.com>
2022-03-03 13:52:35 -0300

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,16 @@
+# \t é a Tabulação horizontal (TAB)
+tabby_cat = "\tI'm tabbed in.\n"
+# \n faz com que a sequencia da string 'on a line' va para linha de baixo
+persian_cat = "I'm split\non a line."
+# \\ cria uma Barra invertida (\)
+backslash_cat = "I'm \\ a \\ cat."
+# as aspas criam uma string que o conteudo é do inicio ao fim das aspas
+fat_cat = """
+I'll do a list:
+\t* Cat food\n\t* Fishies\n\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
