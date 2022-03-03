620ec7b4d3cdda3eba4e3ddd11bdf271efe65fe0
DevTadeu <99771087+DevTadeu@users.noreply.github.com>
2022-03-03 13:55:20 -0300

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+# leia a seção O que Você Deve Ver para saber como executar isso
+script,first, second, third, fourth = argv
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+print("Your fourth variable is:", fourth)
+
+
+input('verde')
